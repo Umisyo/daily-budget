52b2a154388f2190a1ad8cd2224758d499f610da
Umisyo <kusunokisouta@gmail.com>
2025-12-12 14:15:05 +0900

    feat: Add VitePWA plugin to Vite configuration for PWA support and update package.json with vite-plugin-pwa dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import {VitePWA} from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
@@ -5,7 +6,41 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+    injectRegister: 'auto',
+    manifest: {
+      name: 'Daily Budget',
+      short_name: 'Daily Budget', 
+      description: 'Daily Budget',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'vite.svg',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'vite.svg',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'vite.svg',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'vite.svg',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
